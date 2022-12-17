fc86b6b6cc8ce2bb3b973411c706dff420c8052d
ezds98 <70646859+ezds98@users.noreply.github.com>
2022-12-17 10:40:47 +0545

    initial completed

---
+++ public/jquery.js
@@ -0,0 +1,51 @@
+
+
+
+        ///*** $(document).ready(function() {
+        //  $('#register').click(function(e) {
+        //     var firstname = $('#firstname').val();
+        //     var email = $('#email').val();
+        //     var name_regex = /^[a-zA-Z]+$/;
+        //     var email_regex = /^[w-.+]+@[a-zA-Z0-9.-]+.[a-zA-z0-9]{2,4}$/;
+
+        //      if (firstname.length == 0) {
+        //          $('#head').text("* All fields are mandatory *");
+        //          $("#firstname").focus();
+        //         return false;
+        //      }
+        //      else if (!firstname.match(name_regex) || firstname.length == 0) {
+        //           $('#p1').text("* For your name please use alphabets only *");
+        //           $("#firstname").focus();
+        //          return false;
+        //       }
+        //       else if (!lastname.match(name_regex) || lastname.length == 0) {
+        //          $('#p1').text("* For your name please use alphabets only *");
+        //         $("#lastname").focus();
+        //      return false;
+        //    }
+        //  })
+        // })
+
+
+
+
+      //  $("form[name='Register Now']").validate({
+      //      rules: {
+      //          firstname: "required",
+       //         lastname: "required",
+        //        email: {
+        //            required: true,
+        //            email: true,
+       //         },
+       //     },
+        //    messages: {
+       //         firstname: "Please enter your firstname",
+        //        lastname: "Please enter your lastname",
+        //        },
+        //        email: "Please enter a valid email address"
+        //    },
+         //   submitHandler: function(form) {
+        //        form.submit();
+         //   }
+       // });
+   // });
